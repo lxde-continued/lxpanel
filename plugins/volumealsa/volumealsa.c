5c5a7f0aacc356edc2fd77ddc46afeb7f53ff7a9
Salguero Nicolas <nicolas@phenix.salguero.fr>
2022-07-20 09:16:17 +0200

    Fix issue #45

--- plugins/volumealsa/volumealsa.c
+++ plugins/volumealsa/volumealsa.c
@@ -796,6 +796,15 @@ static void volumealsa_popup_scale_scrolled(GtkScale * scale, GdkEventScroll * e
     /* Dispatch on scroll direction to update the value. */
     if ((evt->direction == GDK_SCROLL_UP) || (evt->direction == GDK_SCROLL_LEFT))
         val += 2;
+    else if (evt->direction == GDK_SCROLL_SMOOTH)
+    {
+        gdouble delta_x, delta_y;
+        gdk_event_get_scroll_deltas(evt, &delta_x, &delta_y);
+        if ((delta_x < 0) || (delta_y < 0))
+            val += 2;
+        else
+            val -= 2;
+    }
     else
         val -= 2;
 
@@ -972,6 +981,7 @@ static GtkWidget *volumealsa_constructor(LXPanel *panel, config_setting_t *setti
     /* Allocate top level widget and set into Plugin widget pointer. */
     vol->panel = panel;
     vol->plugin = p = gtk_event_box_new();
+    gtk_widget_add_events(p, GDK_SCROLL_MASK);
     vol->settings = settings;
     lxpanel_plugin_set_data(p, vol, volumealsa_destructor);
     gtk_widget_set_tooltip_text(p, _("Volume control"));
